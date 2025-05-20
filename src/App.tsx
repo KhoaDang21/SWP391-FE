1aa3492073d0acc87ef700031b1da70ca3a29be7
KhoaDang21 <khoadang210903@gmail.com>
2025-05-20 10:45:19 +0700

    chore: setup frontend structure

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <h1 className='text-3xl font-bold underline bg-red-50 text-blue-700'>Hello world!</h1>
+    </>
+  );
+}
+
+export default App;
